139d3e66301a08db7d0a62899110ae9c84809c95
Khaled Alnaeem <khaled-554@hotmail.com>
2025-11-19 22:12:06 +0300

    connection to database

---
+++ src/main/java/com/raseed/raseedbank/RaseedbankApplication.java
@@ -0,0 +1,13 @@
+package com.raseed.raseedbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaseedbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaseedbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raseed/raseedbank/RaseedbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raseed.raseedbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaseedbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
